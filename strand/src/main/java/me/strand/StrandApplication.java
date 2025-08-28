a57216f552af324c574c260c5857663c0dc5463d
TomislavS23 <senjanin.tomislav@gmail.com>
2025-08-28 09:12:19 +0200

    initial setup

---
+++ strand/src/main/java/me/strand/StrandApplication.java
@@ -0,0 +1,13 @@
+package me.strand;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StrandApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StrandApplication.class, args);
+	}
+
+}
---
+++ strand/src/test/java/me/strand/StrandApplicationTests.java
@@ -0,0 +1,13 @@
+package me.strand;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StrandApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
